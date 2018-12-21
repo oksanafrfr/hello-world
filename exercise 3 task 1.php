86900ea05f78f1cd169acc7b933e4768e5fdf802
oksanafrfr <44871805+oksanafrfr@users.noreply.github.com>
2018-12-21 16:30:57 +0400

    нормальная таблица

--- exercise 3 task 1.php
+++ exercise 3 task 1.php
@@ -1,4 +1,4 @@
-<meta charset="UTF-8">
+ <meta charset="UTF-8">
 <pre>
 <?php
     $host = '127.0.0.1';
@@ -6,7 +6,6 @@
     $user = 'student';
     $pass = 'student';
     $charset = 'utf8';
-
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -16,11 +15,12 @@
     $pdo = new PDO($dsn, $user, $pass, $opt);
     $stmt = $pdo->query('SELECT goods, price, quantity, quantity * price AS cost FROM  `112_tts_bill_content` ');
     print "<table border=1>";
+    echo '<tr><td>товар: </td><td>цена: </td><td>количество: </td><td>стоимость: </td>';
     foreach($stmt as $row){
-        echo '<tr><td>товар: </td><td>'.$row['goods'].'</td>';
-        echo '<td>цена: </td><td>'.$row['price'].'</td>';
-        echo '<td>количество: </td><td>'.$row['quantity'].'</td>';
-        echo '<td>стоимость: </td><td>'.$row['cost'].'</td></tr>';
+        echo '<tr><td>'.$row['goods'].'</td>';
+        echo '<td>'.$row['price'].'</td>';
+        echo '<td>'.$row['quantity'].'</td>';
+        echo '<td>'.$row['cost'].'</td></tr>';
     }
     print "</table>";
 ?>
